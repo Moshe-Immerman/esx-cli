27a3eecea785f2099352935e6e268ef2fc7ba2d3
Moshe Immerman <moshe@egis-software.com>
2017-03-04 19:34:09 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import find_packages, setup
+
+requirements = [
+    'Click',
+    'paramiko',
+    'pyvmomi',
+    'requests',
+    'ansicolors',
+    'progressbar2',
+    'termcolor',
+    'colorama',
+    'watchdog'
+]
+
+if os.name == 'posix':
+    requirements.append('sh')
+
+
+setup(name = "esx-cli",
+    install_requires = requirements,
+    version = "0.1",
+    description = "A command line tool for managing vCenter and ESXi servers",
+    author = "Moshe Immerman",
+    author_email = 'name.surname@gmail.com',
+    platforms = ["any"],
+    license = "BSD",
+    url = "http://github.com/Moshe-Immerman/esx-cli",
+    packages = find_packages(),
+    entry_points = {
+        "console_scripts": [
+            "esx = esx_cli.ESX:main",
+        ]
+    }
+)
